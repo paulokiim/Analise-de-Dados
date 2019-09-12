0740a92f087a661fd240e3b246a7aca17f81f917
15483114 <15483114.ik@usp.br>
2019-09-12 00:12:04 -0300

    tinha mudado o pca, tirei as mudanças

--- pca.py
+++ pca.py
@@ -6,7 +6,6 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.decomposition import PCA
-from sklearn import datasets
 
 PATH = "iris.data"
 
@@ -120,37 +119,3 @@ plt.title('PCA of IRIS dataset')
 plt.grid()
 plt.show()
 
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
-
-# In general, it's a good idea to scale the data prior to PCA.
-scaler = StandardScaler()
-scaler.fit(X)
-X=scaler.transform(X)    
-
-pca = PCA()
-x_new = pca.fit_transform(X)
-
-def myplot(score,coeff,labels=None):
-    xs = score[:,0]
-    ys = score[:,1]
-    n = coeff.shape[0]
-    scalex = 1.0/(xs.max() - xs.min())
-    scaley = 1.0/(ys.max() - ys.min())
-    plt.scatter(xs * scalex,ys * scaley, c = y)
-    for i in range(n):
-        plt.arrow(0, 0, coeff[i,0], coeff[i,1],color = 'r',alpha = 0.5)
-        if labels is None:
-            plt.text(coeff[i,0]* 1.15, coeff[i,1] * 1.15, "Var"+str(i+1), color = 'g', ha = 'center', va = 'center')
-        else:
-            plt.text(coeff[i,0]* 1.15, coeff[i,1] * 1.15, labels[i], color = 'g', ha = 'center', va = 'center')
-    plt.xlim(-1,1)
-    plt.ylim(-1,1)
-    plt.xlabel("PC{}".format(1))
-    plt.ylabel("PC{}".format(2))
-    plt.grid()
-
-#Call the function. Use only the 2 PCs.
-myplot(x_new[:,0:2],np.transpose(pca.components_[0:2, :]))
-plt.show()
