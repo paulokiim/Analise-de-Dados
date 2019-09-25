89a41f4174c8b6c2c23866beff387b7d70b127a6
Kim <paulo.jin.sol.kim@accenture.com>
2019-09-25 16:07:47 -0300

    Adicionei a rotacao promax

--- EP-2/exploratoria.py
+++ EP-2/exploratoria.py
@@ -4,6 +4,8 @@ from factor_analyzer import FactorAnalyzer
 import matplotlib.pyplot as plt
 from factor_analyzer.factor_analyzer import calculate_bartlett_sphericity
 from factor_analyzer.factor_analyzer import calculate_kmo
+from sklearn import svm
+from sklearn.model_selection import train_test_split
 
 PATH = 'bfi.csv'
 
@@ -18,7 +20,7 @@ data.dropna(inplace=True)
 # Teste da Esferacidade de Bartlett!!
 chi_square_value, p_value = calculate_bartlett_sphericity(data)
 print(chi_square_value, p_value)
-# p_value = 0, entao podemos proceder com a analise de fatores
+# p_value = 0, entao podemos proceder com a analise de fatores pois quanto menor esse valor, mais confianca temos nas predicoes
 
 # Teste de Kiaser-Meyer-Olkin(KMO)
 kmo_all, kmo_model = calculate_kmo(data)
@@ -44,5 +46,32 @@ plt.show()
 # Criamos um objeto analise de faotres com rotacao varimax
 analisador_varimax = FactorAnalyzer(n_factors=5, rotation="varimax")
 analisador_varimax.fit(data)
+
+autovalores_varimax, v = analisador_varimax.get_eigenvalues()
+print (autovalores_varimax)
 # Nesta linha conseguimos ver que a variancia cumulativa chega a 42% com 5 fatores
 print(analisador_varimax.get_factor_variance())
+
+# Criamos um objeto analise de faotres com rotacao quartimax
+analisador_quartimax = FactorAnalyzer(n_factors=5, rotation="quartimax")
+analisador_quartimax.fit(data)
+
+autovalores_quartimax, v = analisador_quartimax.get_eigenvalues()
+print (autovalores_quartimax)
+# Nesta linha conseguimos ver que a variancia cumulativa chega a 42% com 5 fatores
+print(analisador_quartimax.get_factor_variance())
+
+# Criamos um objeto analise de faotres com rotacao promax
+analisador_promax = FactorAnalyzer(n_factors=5, rotation="promax")
+analisador_promax.fit(data)
+
+autovalores_promax, v = analisador_promax.get_eigenvalues()
+print (autovalores_promax)
+# Nesta linha conseguimos ver que a variancia cumulativa chega a 41% com 5 fatores
+print(analisador_promax.get_factor_variance())
+
+#Podemos observar que a variancia cumulativa para Quartimax e Varimax(Ortogonal) sao as mesmas porem para Promax(Obliquo) é diferente!
+
+X_train, X_test, y_train, y_test = train_test_split(data, data.columns, test_size=0.4, random_state=0)
+
+print (X_train.shape, y_train.shape)
