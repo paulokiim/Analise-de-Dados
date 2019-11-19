f02b65589db9b4daca36b8399ce025b5dec3a131
Paulo <paulojskim@usp.br>
2019-11-19 09:23:31 -0300

    start

--- RandomForest/trab.py
+++ RandomForest/trab.py
@@ -1,3 +1,50 @@
+#%%
 import pandas as pd
 import numpy as np
-import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+
+PATH = 'heart.csv'
+
+dt = pd.read_csv(PATH)
+
+dt.columns = ['age', 'sex', 'chest_pain_type', 'resting_blood_pressure', 'cholesterol', 'fasting_blood_sugar', 'rest_ecg', 'max_heart_rate_achieved',
+       'exercise_induced_angina', 'st_depression', 'st_slope', 'num_major_vessels', 'thalassemia', 'target']
+
+dt['sex'][dt['sex'] == 0] = 'female'
+dt['sex'][dt['sex'] == 1] = 'male'
+
+dt['chest_pain_type'][dt['chest_pain_type'] == 1] = 'typical angina'
+dt['chest_pain_type'][dt['chest_pain_type'] == 2] = 'atypical angina'
+dt['chest_pain_type'][dt['chest_pain_type'] == 3] = 'non-anginal pain'
+dt['chest_pain_type'][dt['chest_pain_type'] == 4] = 'asymptomatic'
+
+dt['fasting_blood_sugar'][dt['fasting_blood_sugar'] == 0] = 'lower than 120mg/ml'
+dt['fasting_blood_sugar'][dt['fasting_blood_sugar'] == 1] = 'greater than 120mg/ml'
+
+dt['rest_ecg'][dt['rest_ecg'] == 0] = 'normal'
+dt['rest_ecg'][dt['rest_ecg'] == 1] = 'ST-T wave abnormality'
+dt['rest_ecg'][dt['rest_ecg'] == 2] = 'left ventricular hypertrophy'
+
+dt['exercise_induced_angina'][dt['exercise_induced_angina'] == 0] = 'no'
+dt['exercise_induced_angina'][dt['exercise_induced_angina'] == 1] = 'yes'
+
+dt['st_slope'][dt['st_slope'] == 1] = 'upsloping'
+dt['st_slope'][dt['st_slope'] == 2] = 'flat'
+dt['st_slope'][dt['st_slope'] == 3] = 'downsloping'
+
+dt['thalassemia'][dt['thalassemia'] == 1] = 'normal'
+dt['thalassemia'][dt['thalassemia'] == 2] = 'fixed defect'
+dt['thalassemia'][dt['thalassemia'] == 3] = 'reversable defect'
+
+dt['sex'] = dt['sex'].astype('object')
+dt['chest_pain_type'] = dt['chest_pain_type'].astype('object')
+dt['fasting_blood_sugar'] = dt['fasting_blood_sugar'].astype('object')
+dt['rest_ecg'] = dt['rest_ecg'].astype('object')
+dt['exercise_induced_angina'] = dt['exercise_induced_angina'].astype('object')
+dt['st_slope'] = dt['st_slope'].astype('object')
+dt['thalassemia'] = dt['thalassemia'].astype('object')
+
+dt
+
+# %%
